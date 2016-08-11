3cb0cb68bac18d50fb93e952eaff256a45f385c5
nmajumder <nathan.majumder@tufts.edu>
2016-08-10 21:15:15 -0400

    reverting to html

--- head.php
+++
@@ -1,9 +0,0 @@
-<head>
-	<meta charset="utf-8">
-	<title>Nathan Majumder</title>
-	<link rel="stylesheet" href="css/normalize.css">
-	<link href='http://fonts.googleapis.com/css?family=Josefin+Sans:400,600|Exo:400,600,600italic|Muli' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="css/mystyle.css">
-	<link rel="stylesheet" href="css/responsive.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
