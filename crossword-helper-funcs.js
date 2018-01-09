39c2e53a9b2ea2a8ac2454311da3ee6bff78d0d9
nmajumder <nathan.majumder@tufts.edu>
2018-01-09 11:29:26 -0500

    revert to not checking for font availability

--- crossword-helper-funcs.js
+++ crossword-helper-funcs.js
@@ -834,59 +834,6 @@ document.onkeydown = function(evt) {
 };
 
 
-
-/*****************************************************************************
-/* TAKEN FROM WEBPAGE https://www.samclarke.com/javascript-is-font-available/
-/*****************************************************************************/
-/**
- * Checks if a font is available to be used on a web page.
- *
- * @param {String} fontName The name of the font to check
- * @return {Boolean}
- * @license MIT
- * @copyright Sam Clarke 2013
- * @author Sam Clarke <sam@samclarke.com>
- */
-(function (document) {
-  var width;
-  var body = document.body;
-
-  var container = document.createElement('span');
-  container.innerHTML = Array(100).join('wi');
-  container.style.cssText = [
-    'position:absolute',
-    'width:auto',
-    'font-size:128px',
-    'left:-99999px'
-  ].join(' !important;');
-
-  var getWidth = function (fontFamily) {
-    container.style.fontFamily = fontFamily;
-
-    body.appendChild(container);
-    width = container.clientWidth;
-    body.removeChild(container);
-
-    return width;
-  };
-
-  // Pre compute the widths of monospace, serif & sans-serif
-  // to improve performance.
-  var monoWidth  = getWidth('monospace');
-  var serifWidth = getWidth('serif');
-  var sansWidth  = getWidth('sans-serif');
-
-  window.isFontAvailable = function (font) {
-    return monoWidth !== getWidth(font + ',monospace') ||
-      sansWidth !== getWidth(font + ',sans-serif') ||
-      serifWidth !== getWidth(font + ',serif');
-  };
-})(document);
-
-/*******************************************************/
-
-
-
 function getClueNum(obj) {
     var txt = obj.innerHTML;
     var pos1 = txt.indexOf(">");
